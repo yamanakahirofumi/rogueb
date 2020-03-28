71e0ae29895eced08e98cd9c3271f43105d6c0c3
hiro <mokojarasi@gmail.com>
2020-03-28 23:10:02 +0900

    first commit

---
+++ src/test/java/net/hero/rogueb/RoguebApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hero.rogueb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoguebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
