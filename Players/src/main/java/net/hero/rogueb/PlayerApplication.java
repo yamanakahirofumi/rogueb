2eb18cd31e1faf5cad4f892150ac1090a48a21de
Hirofumi Yamanaka <mokojarasi@gmail.com>
2021-02-01 00:29:37 +0900

    フォルダ整理

--- Players/src/main/java/net/hero/rogueb/PlayerApplication.java
+++ Players/src/main/java/net/hero/rogueb/PlayerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoguebApplication {
+public class PlayerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RoguebApplication.class, args);
+        SpringApplication.run(PlayerApplication.class, args);
     }
 
 }
